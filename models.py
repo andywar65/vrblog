b24c2efd746c8b3ae2677b1ba1ef57b424334253
andywar65 <andy.war1965@gmail.com>
2017-08-12 00:19:24 +0200

    posts reordered and images added

--- models.py
+++ models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
-from wagtail.wagtailcore.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailsearch import index
 
 class VrblogIndexPage(Page):
     intro = RichTextField(blank=True)
+
+    def get_context(self, request):
+        #Update context to include only published posts, in reverse order
+        context = super(VrblogIndexPage, self).get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
 
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full"),
@@ -17,6 +27,13 @@ class VrblogPage(Page):
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
 
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
         index.SearchField('body'),
@@ -26,4 +43,17 @@ class VrblogPage(Page):
         FieldPanel('date'),
         FieldPanel('intro'),
         FieldPanel('body', classname="full"),
+        InlinePanel('gallery_images', label="Gallery images"),
+    ]
+
+class VrblogPageGalleryImage(Orderable):
+    page = ParentalKey(VrblogPage, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
     ]
